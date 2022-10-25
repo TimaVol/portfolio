f6b367c0732106ada096aa62a2d5499d7d65eb99
TimaVol <tima.voloshuk@gmail.com>
2022-10-25 16:10:13 +0300

    clear console log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,8 +14,6 @@ import { loadFull } from 'tsparticles'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine)
-
     // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
@@ -23,9 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [])
 
   const particlesLoaded = useCallback(
-    async (container: Container | undefined) => {
-      await console.log(container)
-    },
+    async (container: Container | undefined) => {},
     []
   )
 
