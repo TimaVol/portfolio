a2d78fa7b1607cdfba2d0dc0e61b47768121b6e9
TimaVol <tima.voloshuk@gmail.com>
2022-12-07 17:23:22 +0200

    update nextjs to 13 version

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,39 +1,39 @@
-import { deleteDoc, doc, getDoc } from "firebase/firestore";
-import Image from "next/image";
-import React from "react";
-import { db, storage } from "../common/firebase";
-import styles from "../styles/components/Dashboard.module.scss";
-import { deleteObject, ref } from "firebase/storage";
-import { Item } from "../common/types";
+import { deleteDoc, doc, getDoc } from 'firebase/firestore'
+import Image from 'next/legacy/image'
+import React from 'react'
+import { db, storage } from '../common/firebase'
+import styles from '../styles/components/Dashboard.module.scss'
+import { deleteObject, ref } from 'firebase/storage'
+import { Item } from '../common/types'
 
 interface DashboardProps {
-  items?: Item[];
-  deleteFunc?: boolean;
+  items?: Item[]
+  deleteFunc?: boolean
 }
 
 export default function Dashboard({ items, deleteFunc }: DashboardProps) {
   const deleteItemHandler = async (id: string) => {
-    console.log(id);
+    console.log(id)
 
     try {
-      const item = (await getDoc(doc(db, "items", id))).data();
-      const imgName = item?.imgName;
+      const item = (await getDoc(doc(db, 'items', id))).data()
+      const imgName = item?.imgName
 
-      const imgRef = ref(storage, imgName);
+      const imgRef = ref(storage, imgName)
 
       await deleteObject(imgRef)
         .then(() => {
-          console.log("File deleted successfully");
+          console.log('File deleted successfully')
         })
         .catch((error) => {
-          throw new Error(error);
-        });
+          throw new Error(error)
+        })
 
-      const deletedItem = await deleteDoc(doc(db, "items", id));
+      const deletedItem = await deleteDoc(doc(db, 'items', id))
     } catch (error) {
-      throw new Error(`${error}`);
+      throw new Error(`${error}`)
     }
-  };
+  }
 
   if (!items)
     return (
@@ -43,13 +43,13 @@ export default function Dashboard({ items, deleteFunc }: DashboardProps) {
         <div></div>
         <div></div>
       </div>
-    );
+    )
 
   return (
     <div className={styles.dashboard}>
       {items?.map((item) => (
         <div key={item.id} className={styles.item}>
-          <a href={item.link} target={"_blank"} rel="noreferrer">
+          <a href={item.link} target={'_blank'} rel="noreferrer">
             <h4>{item.title}</h4>
             <div className={styles.img}>
               <Image src={item.imgUrl} alt="item img" layout="fill" priority />
@@ -66,5 +66,5 @@ export default function Dashboard({ items, deleteFunc }: DashboardProps) {
         </div>
       ))}
     </div>
-  );
+  )
 }
