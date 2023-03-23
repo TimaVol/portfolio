22c43e205715dee5f6c46d06072ba417ab1c6b97
TimaVol <tima.voloshuk@gmail.com>
2023-03-23 22:39:41 +0200

    wip

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document"
-
-export default function Document() {
-  return (
-    <Html lang="en" className="dark">
-      <Head />
-      <body className="bg-gray transition-all dark:bg-blackLight">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import Document, { Html, Head, Main, NextScript } from "next/document"
+import i18nextConfig from "../next-i18next.config"
+class MyDocument extends Document {
+  render(): JSX.Element {
+    const currentLocale =
+      this.props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale
+    return (
+      <Html lang={currentLocale} className="dark">
+        <Head />
+        <body className="bg-gray transition-all dark:bg-blackLight">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+export default MyDocument
